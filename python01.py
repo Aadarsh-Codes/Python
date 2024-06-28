3039a6344d446f340e428cc33cde5e258f49adb3
Aadarsh-Codes <nanawareaadarsh333@gmail.com>
2024-06-29 00:06:12 +0530

    06/29/2024   12:07 AM

---
+++ python01.py
@@ -0,0 +1,13 @@
+'''
+@file:- python01.py
+@Descripition :- writing our 1st program in python
+@Author :- Aadarsh Nanaware(nanawareaadarsh7@gmail.com)
+@Date :- 02/05/2024
+'''
+
+
+
+# First Program in python
+
+print("Hello , World!")
+print(2**100)
