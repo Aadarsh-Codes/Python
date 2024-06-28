6bf3cbedc3d3b38d143563992b777b6e2a10af08
Aadarsh-Codes <nanawareaadarsh333@gmail.com>
2024-06-29 00:08:49 +0530

    06/29/2024  12:08 AM

---
+++ printing_statement.py
@@ -0,0 +1,19 @@
+'''
+@file:- printing_statement.py
+@Descripition :- In deep of printing () function
+@Author :- Aadarsh Nanaware(nanawareaadarsh7@gmail.com)
+@Date :- 02/05/2024
+'''
+
+# Basic Usage
+print("welcome")
+
+#  printing Multiple values
+print("Hello",5,2**100)
+
+#  seperstor (sep)
+print("Hello","Aadarsh",sep = " ,, " )
+
+# END (end)
+print("hello", end = "!")
+print("how are you")
